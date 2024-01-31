6a6f6438cec4aca2c386ea940c5359a79626d049
Palesa <devnhack@gmail.com>
2024-01-31 23:00:47 +0200

    latest

---
+++ module_1/pipeline.py
@@ -0,0 +1,16 @@
+import sys
+
+#print the arg at position 1
+print(sys.argv[0])
+
+#create a variable
+day = sys.argv[1]
+
+
+
+import pandas as pd
+
+
+#do fancy thing below
+
+print(f'job finished successfully for this day ={day}' )
